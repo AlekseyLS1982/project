c7266d6e9baf884852bce42892ddef66a2350c85
Alex <asafutin@gmail.com>
2021-12-07 23:27:05 +0300

    тест

--- test/js3/js/script.js
+++ test/js3/js/script.js
@@ -18,41 +18,69 @@ const movieDB = {
 Для получения доступа к значению input - обращаемся к нему как input.value;
 P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.*/
 
-
-
-
-
-
-const input = document.querySelector('.adding__input');
 const ul = document.querySelector('.promo__interactive-list');
-const button = document.querySelector('.add').lastElementChild;
-const liDelete = document.querySelector('.delete');
-let newLi = document.createElement('li');
+let input = document.querySelector('.adding__input');
+const button = document.querySelector('button');
 
 
 
-function addElem(){  
-    newLi.classList = 'promo__interactive-item';
-    newLi.append(liDelete);
-    ul.append(newLi);
-    input.value = '';
-}
 
-
-button.addEventListener('click', function(event){
+button.addEventListener('click',function(event){
     event.preventDefault();
-    console.log(input.value.length);
-    if(input.value !== "" && input.length <= 21) {
+    const newLi = document.createElement('li');
+    const newDiv = document.createElement('div');
+
+    newLi.classList = 'promo__interactive-item';
+    if(input.value.length < 21) {
         newLi.textContent = input.value;
-        addElem();
-    } else if (input.value !=="" && input.value.length > 21) {
-        newLi.textContent = input.value.slice(0,21) +'...';
-        addElem();
-    } else {
-        console.log('введите фильм');
+    } else if (input.value.length >= 21) {
+        let i = input.value;
+        let x =i.slice(0,21);
+        x = x+'...';
+        newLi.textContent = x;
     }
     
+    newDiv.classList = 'delete';
+
+    newLi.append(newDiv);
+    ul.append(newLi);
+
+    input.value = '';
+
+
+    let trueInput = document.querySelectorAll('input');
+
+    trueInput.forEach(function(item,i){
+        if(i==2) {
+            trueInput = item;
+        } 
+    });
+    if(trueInput.value == 'on'){
+        console.log('добовляем любимый фильм');
+    } else if(trueInput.values != 'on'){
+
+    }
+
+    
+    
 });
+
+ul.addEventListener('mouseover',function(){
+    const divDelete = document.querySelectorAll('.delete');
+    divDelete.forEach(function(item){
+        item.addEventListener('click',function(){
+            let z = item.parentElement;
+            z.remove();
+        });
+    });
+});
+
+
+
+
+
+
+
     
 
 
