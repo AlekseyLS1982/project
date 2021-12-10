52c4182a3fbccad33d5eddc966fefc3274369368
Alex <asafutin@gmail.com>
2021-12-10 20:18:03 +0300

    js тренеровка

--- test/js3/js/script.js
+++ test/js3/js/script.js
@@ -1,7 +1,106 @@
 
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
 
-const movieDB = {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = document.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if(newFilm) {
+
+            if(newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0,22)}...`;
+            }
+            if(favorite) {
+                console.log("добовляем любимый фильм");
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            createMovieList(movieDB.movies, movieList);
+    
+        }
+        event.target.reset();
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+    
+   
+
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+   
+
+   
+    function createMovieList(films, parent) {
+
+        parent.innerHTML = "";
+        sortArr(films);
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class = "promo__interactive-item">${i + 1} ${film}
+                    <div class = "delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn,i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        });
+
+
+    }
+    
+
+    deleteAdv(adv);
+    makeChanges();
+    
+    createMovieList(movieDB.movies, movieList);
+});
+
+
+
+/*const movieDB = {
     movies: [
         "Логан",
         "Лига справедливости",
@@ -9,14 +108,14 @@ const movieDB = {
         "Одержимость",
         "Скотт Пилигрим против..."
     ]
-};
+};*/
 
 /*
 1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
 новый фильм добавляется в список. Страница не должна перезагружаться.
 Новый фильм должен добавляться в movieDB.movies.
 Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.*/
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
 const ul = document.querySelector('.promo__interactive-list');
 let input = document.querySelector('.adding__input');
@@ -73,7 +172,7 @@ ul.addEventListener('mouseover',function(){
             z.remove();
         });
     });
-});
+});*/
 
 
 
